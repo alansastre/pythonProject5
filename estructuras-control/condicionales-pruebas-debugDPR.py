b5e0df14de5d43a235110df4ddbdb448817e5860
DanielPerezRos <ahhrrc@gmail.com>
2021-04-25 23:09:13 +0200

    Archivo de prueba de debug DPR

---
+++ estructuras-control/condicionales-pruebas-debugDPR.py
@@ -0,0 +1,18 @@
+check1 = False
+check2 = False
+check3 = False
+check4 = False
+
+if check1:
+    print("check1")
+elif check2:
+    print("check2")
+elif check3:
+    print("check3")
+elif check4:
+    print("check4")
+else:
+    print("else")
+
+check4 = True
+print("fin pruebas depuración")
